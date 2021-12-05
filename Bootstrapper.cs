443da395ba1e24ca5b5020668bbd2e43f203a4cd
Khai Hoan <khaihoan2306@gmail.com>
2021-12-05 19:34:50 +0700

    Create database and login function

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using Household_Management_System.Helpers;
 using Household_Management_System.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Household_Management_System
 {
@@ -14,6 +16,11 @@ namespace Household_Management_System
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
---
+++ DataAccess/SqliteDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Household_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Household_Management_System.DataAccess
+{
+    public class SqliteDataAccess
+    {
+        public static List<LocalPoliceModel> LoadPolice(string username)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<LocalPoliceModel>("select * from LocalPolice where Username='"+username+"'", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+        private static string LoadConnectionString(string id = "Default")
+        {
+            string connectionString = "Data Source=";
+            string dir = Environment.CurrentDirectory;
+            dir = dir.Remove(dir.Length - 9, 9);
+            dir += "HouseholdDB.db";
+            connectionString += dir + ";Version=3;";
+            return connectionString;                
+        }
+    }
+}
