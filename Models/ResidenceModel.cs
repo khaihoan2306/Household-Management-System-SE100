332451e89c0f995b5209bdd8b6ad20166d00eaff
NguyenTruong224 <32810848+NguyenTruong224@users.noreply.github.com>
2021-12-14 01:41:44 +0700

    Add ResidenceModel

--- Models/ResidenceModel.cs
+++ Models/ResidenceModel.cs
@@ -6,19 +6,18 @@ using System.Threading.Tasks;
 
 namespace Household_Management_System.Models
 {
-    public class AbsenceModel
+    public class ResidenceModel
     {
-        private string absenceName, identityCode, birthDay, address, gender, toDay, fromDay, reasonAbsence;
-        public AbsenceModel(string absenceName, string identityCode, string birthDay,  string gender, string address, string toDay, string fromDay, string reasonAbsence)
+        private string absenceName, identityCode, birthDay, address, gender, shelter, arrivalDate;
+        public ResidenceModel(string absenceName, string identityCode, string birthDay, string gender, string address, string shelter, string arrivalDate)
         {
             this.AbsenceName = absenceName;
             this.IdentityCode = identityCode;
             this.BirthDay = birthDay;
             this.Address = address;
             this.Gender = gender;
-            this.ToDay = toDay;
-            this.FromDay = fromDay;
-            this.ReasonAbsence = reasonAbsence;
+            this.Shelter = shelter;
+            this.ArrivalDate = arrivalDate;
         }
 
         public string AbsenceName { get => absenceName; set => absenceName = value; }
@@ -26,9 +25,8 @@ namespace Household_Management_System.Models
         public string BirthDay { get => birthDay; set => birthDay = value; }
         public string Address { get => address; set => address = value; }
         public string Gender { get => gender; set => gender = value; }
-        public string ToDay { get => toDay; set => toDay = value; }
-        public string FromDay { get => fromDay; set => fromDay = value; }
-        public string ReasonAbsence { get => reasonAbsence; set => reasonAbsence = value; }
+        public string Shelter { get => shelter; set => shelter = value; }
+        public string ArrivalDate { get => arrivalDate; set => arrivalDate = value; }
     }
 }
 
